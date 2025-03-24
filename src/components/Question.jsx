fea22b01052f40bda1e2fd93abaf6e32cbfb901d
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-24 16:43:48 -0500

    feat(components): add Question component with GitHub link button
    This commit introduces a new Question component that includes a button linking to the GitHub profile. The button triggers a new tab opening the specified URL when clicked. The component is styled using Tailwind CSS for responsiveness and interactivity.

---
+++ src/components/Question.jsx
@@ -0,0 +1,19 @@
+function Question() {
+    
+    const handleGithub = () => {
+        window.open("https://github.com/JunLovin", "_blank")
+    }
+
+    return (
+        <>
+        <div className="question h-[50dvh] mt-50">
+            <div className="question-container h-full w-full flex flex-col justify-center items-center gap-10">
+                <h2 className="font-poppins font-extrabold text-5xl">Ready To Build Your Community?</h2>
+                <button className="bg-pink-400 text-white w-max px-20 py-5 rounded-full shadow-2xl cursor-pointer font-bold hover:-translate-y-1 transition-all duration-200 active:translate-y-1 font-open-sans" onClick={handleGithub}>Get Started For Free</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Question
