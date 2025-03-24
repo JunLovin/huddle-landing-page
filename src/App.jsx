9d4b9669d3fa1f69e5d75fa730615a9dbb761b7b
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-24 16:42:45 -0500

    feat: add new components to App.jsx for enhanced UI
    This commit introduces new components (`TotalUsers`, `GrowTogether`, `FlowingConversations`, `YourUsers`, `Question`) to the `App.jsx` file to enrich the user interface and improve the overall user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Header from '@components/Header'
 import Hero from '@components/Hero'
+import TotalUsers from '@components/TotalUsers'
+import GrowTogether from '@components/GrowTogether'
+import FlowingConversations from '@components/FlowingConversations'
+import YourUsers from '@components/YourUsers'
+import Question from '@components/Question'
 
 function App() {
   return (
     <>
     <Header />
     <Hero />
+    <TotalUsers />
+    <GrowTogether />
+    <FlowingConversations />
+    <YourUsers />
+    <Question />
     </>
   )
 }
