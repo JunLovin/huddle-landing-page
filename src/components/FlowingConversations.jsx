fd3ad9b94a6b9791b4b572f6e6b07a6c5a9ad8d9
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-24 16:43:07 -0500

    feat: add FlowingConversations component for displaying conversation section
    This commit introduces a new React component, FlowingConversations, which is responsible for rendering a section that highlights the natural flow of conversations. The component includes a title, a descriptive paragraph, and an illustrative SVG image. This addition enhances the user interface by providing a visually appealing and informative section about the platform's conversation features.

---
+++ src/components/FlowingConversations.jsx
@@ -0,0 +1,23 @@
+import FlowingC from '@public/images/illustration-flowing-conversation.svg'
+
+function FlowingConversations() {
+    return (
+        <>
+        <div className="grow-top w-full relative mt-90">
+            <div className="grow h-[50dvh] w-full relative">
+                <div className="grow-container w-full h-full flex flex-row-reverse justify-around items-center">
+                    <div className="grow-left flex flex-col gap-7 h-full justify-center">
+                        <h2 className="font-poppins font-extrabold text-5xl">Flowing Conversations</h2>
+                        <p className="max-w-[60ch] font-open-sans font-medium">You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.</p>
+                    </div>
+                    <div className="grow-right h-full flex justify-center items-center">
+                        <img src={FlowingC} alt=""  className="w-full h-full"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default FlowingConversations
